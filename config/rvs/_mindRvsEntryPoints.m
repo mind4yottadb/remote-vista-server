c9ac050c5d229446d267f0daa12a02cc9afa851f
slalli <stefano@dnasoft.nl>
2026-05-19 14:40:22 +0200

    initial commit

---
+++ config/rvs/_mindRvsEntryPoints.m
@@ -0,0 +1,21 @@
+;#################################################################
+;#                                                               #
+;# Copyright (c) 2026 DnaSoft B.V. and/or its subsidiaries.      #
+;# All rights reserved.                                          #
+;#                                                               #
+;#   This source code contains the intellectual property         #
+;#   of its copyright holder(s), and is made available           #
+;#   under a license.  If you do not know the terms of           #
+;#   the license, please stop and do not read further.           #
+;#                                                               #
+;#################################################################
+;
+; ************************************************************
+;
+; ************************************************************
+; parameters:
+; 1 glvn
+;
+; Returns:
+;
+;
